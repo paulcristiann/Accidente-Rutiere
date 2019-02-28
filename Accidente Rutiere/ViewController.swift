9fe3d67f4ab58b0e544869e92287e1a4a146b974
Paul-Cristian Vasile <vasilepaul97@gmail.com>
2019-02-28 10:07:41 +0200

    Initial Commit

---
+++ Accidente Rutiere/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Accidente Rutiere
+//
+//  Created by Paul-Cristian Vasile on 28/02/2019.
+//  Copyright © 2019 Paul-Cristian Vasile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
